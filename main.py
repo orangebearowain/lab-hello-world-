8648d843d8cac0eec5097c494bbeaa7fdddae9be
orangebearowain <yanowain2010@gmail.com>
2025-07-01 14:50:44 -0500

    New line

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-print("Hello world")
-print("Hello world")
-print("Hello world")
+print("Welcome to AI Applications!")
+name = input("What is your name?")
+print("Hello", name, "It's great to meet you!")
