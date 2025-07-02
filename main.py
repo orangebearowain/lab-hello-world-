13464b0c36f6953b2e9e049ed7fb9af80f184ef4
orangebearowain <yanowain2010@gmail.com>
2025-07-01 21:49:16 -0500

    New

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 numb = input("Please input a number: ")
 numb2 = input("Please input a second number: ")
-oper = input("Please input an operator: ")
+oper = input("Please input an operator (+, -, *, /): ")
 
 if oper == "+":
-    print(int(numb) + int(numb2))
+    print(int(numb) + int(numb2) + 0.00)
 elif oper == "-":
-    print(int(numb) - int(numb2))
+    print(int(numb) - int(numb2) + 0.00)
 elif oper == "*":
-    print(int(numb) * int(numb2))
+    print(int(numb) * int(numb2) + 0.00)
 elif oper == "/":
-    print(int(numb) / int(numb2))
+    if numb2 == "0":
+        print("Can not divide by zero! ")
+    else:
+        print( int(numb) / int(numb2) + 0.00)
+else:
+    print("Invalid operator")
